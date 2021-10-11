f95c16d019bab5dabb647612bb8dcf443d9affd1
Maurice Ammour <maurice.ammour@informatik.hs-fulda.de>
2021-10-11 19:24:28 +0200

    add Button hinzugefügt

--- app.js
+++ app.js
@@ -8,6 +8,47 @@ function fetchTable(table) {
 }
 
 
-var ul = document.getElementById("game_list").innerHTML = "hallo";
-var newObjekt = document.createElement("li");
-ul.appendChild(newObjekt);
+document.getElementById("add_games_button").addEventListener("click", addGame);
+function addGame(){
+    const gameName = document.getElementById("game_name").value;
+    const gameDate = document.getElementById("game_date").value;
+    const gameElement = document.createElement("p");
+
+    gameElement.appendChild(gameName);
+    gameElement.appendChild(deleteGame);
+    document.getElementById("content_games").appendChild(gameElement);
+
+    const game = {
+        name: gameName,
+        release_date: gameDate
+    };
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(game),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    fetch("http://localhost:8080/game/", options)
+        .then(function (res){
+            return res.json();
+        })
+        .then(function (result) {
+        const deleteGame = document.createElement("button");
+        deleteGame.addEventListener('click', function (){
+
+            const options = {
+                method: 'DELETE',
+                body: JSON.stringify({
+                    id: result.id
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+            fetch("http://localhost:8080/game/", options);
+        })
+    });
+
+}
