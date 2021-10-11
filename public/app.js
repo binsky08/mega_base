923230906e87773edfb8d77c52a68b56ab311843
binsky <timo@binsky.org>
2021-10-11 21:05:26 +0200

    add game edit function

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
+const host = 'http://localhost:8081/data';
+
 function fetchTable(table) {
-    fetch('http://localhost:8080/get/' + table).then((res) => {
+    fetch(host + '/get/' + table).then((res) => {
         return res.json();
     }).then((jsonData) => {
         document.getElementById('result').innerText = JSON.stringify(jsonData);
@@ -8,17 +10,25 @@ function fetchTable(table) {
 }
 
 function refreshGameList() {
-    fetch('http://localhost:8080/game').then((res) => {
+    fetch(host + '/game').then((res) => {
         return res.json();
     }).then((jsonData) => {
         const gameList = document.getElementById('game_list');
         gameList.innerHTML = '';
         for (const gameListElement of jsonData) {
             const game = document.createElement('li');
-            game.appendChild(gameListElement.name);
+            const gameName = document.createElement('span');
+            gameName.innerText = gameListElement.name;
+            game.appendChild(gameName);
+
+            const editGame = document.createElement("button");
+            editGame.innerText = 'Edit';
+            editGame.style.marginLeft = '5px';
+            editGame.addEventListener('click', () => loadGameToEdit(gameListElement));
 
             const deleteGame = document.createElement("button");
-            deleteGame.value = 'Delete';
+            deleteGame.innerText = 'Delete';
+            deleteGame.style.marginLeft = '5px';
             deleteGame.addEventListener('click', function () {
                 const options = {
                     method: 'DELETE',
@@ -29,9 +39,10 @@ function refreshGameList() {
                         'Content-Type': 'application/json'
                     }
                 };
-                fetch("http://localhost:8080/game/", options);
+                fetch(host + '/game', options);
             });
 
+            game.appendChild(editGame);
             game.appendChild(deleteGame);
             gameList.appendChild(game);
         }
@@ -39,16 +50,26 @@ function refreshGameList() {
     });
 }
 
-
+refreshGameList();
 document.getElementById("add_games_button").addEventListener("click", addGame);
 
+function loadGameToEdit(gameListElement) {
+    const clone = document.getElementById("edit_games_button").cloneNode(true);
+    document.getElementById("edit_games_button").replaceWith(clone);
+
+    document.getElementById("edit_game_name").value = gameListElement.name;
+    document.getElementById("edit_game_date").value = gameListElement.release_date.slice(0, 10);
+    document.getElementById("edit_game_group").classList.remove('display-none');
+    document.getElementById("edit_games_button").addEventListener('click', () => editGame(gameListElement));
+}
+
 function addGame() {
-    const gameName = document.getElementById("game_name").value;
-    const gameDate = document.getElementById("game_date").value;
+    const gameName = document.getElementById("add_game_name");
+    const gameDate = document.getElementById("add_game_date");
 
     const game = {
-        name: gameName,
-        release_date: gameDate
+        name: gameName.value,
+        release_date: gameDate.value
     };
 
     const options = {
@@ -59,10 +80,43 @@ function addGame() {
         }
     };
 
-    fetch("http://localhost:8080/game/", options)
+    fetch(host + '/game', options)
         .then(function (res) {
             if (res.statusCode === 200) {
                 refreshGameList();
+                gameName.value = '';
+                gameDate.value = '';
+            } else {
+                alert(res.statusMessage);
+            }
+        });
+}
+
+function editGame(gameListElement) {
+    const editGameName = document.getElementById("edit_game_name");
+    const editGameDate = document.getElementById("edit_game_date");
+
+    const game = {
+        id: gameListElement.id,
+        name: editGameName.value,
+        release_date: editGameDate.value
+    };
+
+    const options = {
+        method: 'PATCH',
+        body: JSON.stringify(game),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    fetch(host + '/game', options)
+        .then(function (res) {
+            if (res.statusCode === 200) {
+                refreshGameList();
+                editGameName.value = '';
+                editGameDate.value = '';
+                document.getElementById("edit_game_group").classList.add('display-none');
             } else {
                 alert(res.statusMessage);
             }
